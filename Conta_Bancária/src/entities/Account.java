303c597362ad60cc8622fce4b6a029e03affe8a5
rscopim <ricardo.simines@gmail.com>
2023-07-15 14:18:47 -0300

    Estudo herança e Polimorfismo

---
+++ Conta_Bancária/src/entities/Account.java
@@ -0,0 +1,47 @@
+package entities;
+
+public class Account {
+	
+	private Integer number;
+	private String Holder;
+	private Double balance;
+	
+	public Account() {
+		}
+
+	public Account(Integer number, String holder, Double balance) {
+		this.number = number;
+		this.Holder = holder;
+		this.balance = balance;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public String getHolder() {
+		return Holder;
+	}
+
+	public void setHolder(String holder) {
+		Holder = holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void withdraw(double amount) {
+		balance -= amount;
+	}
+	
+	public void deposit(double amount) {
+		balance += amount;
+	}
+	
+
+}
---
+++ Conta_Bancária/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author user
+ *
+ */
+module Conta_Bancária {
+}
